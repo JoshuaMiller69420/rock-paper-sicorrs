094f6c7e86ad4cc2b0d9a9b2a01278e72e7b96b5
Joshua Miller <jmm.ects@gmail.com>
2024-11-01 10:46:12 -0400

    work in progress

--- Rock Paper Siccors/rock_paper_siccors.py
+++ Rock Paper Siccors/rock_paper_siccors.py
@@ -1,57 +1,48 @@
 import random
 score = 0
 
-def user_1(score):
-    user_in = input("Enter 1 for Rock, 2 for Paper, 3 for Scissors or Q to quit: ").lower()
-    if user_in == "1":
-        return "rock"
-        score += 1
-    elif user_in == "2":
-        return "paper"
-        score += 1
-    elif user_1 == "3":
-        return "scissors"
-        score += 1
-    elif user_1 == "q":
+def player():
+    player_in = input("Enter 1 for Rock, 2 for Paper, 3 for Scissors or Q to quit: ").lower()
+    if player_in == "1":
+        return 1
+    elif player_in == "2":
+        return 2
+    elif player_in == "3":
+        return 3
+    elif player_in == "q":
         exit()
     else:
         print("Wrong number. Try again.")
 
 
-def compu_in():
-    rand_number = random.randint(0, 3)
-    return
-
 def user_2():
     pass
 
+def convert_num_to_word(num):
+    if num == 1:
+        return "rock"
+    elif num == 2:
+        return "paper"
+    elif num == 3:
+        return "scissors"
+
+def computer():
+    rand_number = random.randint(0, 3)
+    return rand_number
+
+
+def check_win():
+    if  ==
+
 
 def round_handler() -> None:
-    
-    print(current_round)
-    user_1()
+    round = 0
+    print(f"Round {round + 1}")
+    player_move = player()
 
 
-def score_counter() -> None: #no return value. Same as void
-    pass
 
-
-def main() -> None:
-    print("""
-     _____            _      _____                         _____      _                        
-    |  __ \\          | |    |  __ \\                       / ____|    (_)                       
-    | |__) |___   ___| | __ | |__) |_ _ _ __   ___ _ __  | (___   ___ _ ___ ___  ___  _ __ ___ 
-    |  _  // _ \\ / __| |/ / |  ___/ _` | '_ \\ / _ \\ '__|  \\___ \\ / __| / __/ __|/ _ \\| '__/ __|
-    | | \\ \\ (_) | (__|   <  | |  | (_| | |_) |  __/ |     ____) | (__| \\__ \\__ \\ (_) | |  \\__ \\
-    |_|  \\_\\___/ \\___|_|\\_\\ |_|   \\__,_| .__/ \\___|_|    |_____/ \\___|_|___/___/\\___/|_|  |___/
-                                       | |                                                     
-                                       |_|     
-        _______           _______               _______ 
-    ---'   ____)      ---'   ____)____     ---'    ____)____ 
-          (_____)               ______)               ______) 
-          (_____)               _______)           __________) 
-          (____)               _______)           (____) 
-    ---.__(___)       ---.__________)      ---.__(___)""")
+def main():
     round_handler()
 
 if __name__ == "__main__":
